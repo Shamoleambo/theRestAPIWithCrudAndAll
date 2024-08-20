fcf032437687a1682a4939ef62bfafbe4f21de6f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-20 10:39:31 -0300

    first commit

---
+++ src/main/java/com/tidz/theRest/TheRestApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.theRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/theRest/TheRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.theRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
