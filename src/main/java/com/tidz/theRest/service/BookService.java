9e931f2f356ad7bf0ffa472f5a50f69525e94575
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-20 10:55:06 -0300

    implement getBookById method

--- src/main/java/com/tidz/theRest/service/BookService.java
+++ src/main/java/com/tidz/theRest/service/BookService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tidz.theRest.errors.BookNotFoundException;
 import com.tidz.theRest.model.Book;
 import com.tidz.theRest.repository.BookRepository;
 
@@ -16,6 +17,10 @@ public class BookService {
 
 	public List<Book> getAllBooks() {
 		return this.repo.findAll();
+	}
+
+	public Book getBookById(Long id) {
+		return this.repo.findById(id).orElseThrow(() -> new BookNotFoundException("Book not found - " + id));
 	}
 
 }
