91fc752faa670d392a3cc7c478a23e6f485db76b
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-20 10:52:25 -0300

    create BookService and implement the getAllBooks method

---
+++ src/main/java/com/tidz/theRest/service/BookService.java
@@ -0,0 +1,21 @@
+package com.tidz.theRest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tidz.theRest.model.Book;
+import com.tidz.theRest.repository.BookRepository;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private BookRepository repo;
+
+	public List<Book> getAllBooks() {
+		return this.repo.findAll();
+	}
+
+}
